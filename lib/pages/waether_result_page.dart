1b21ec7a33988394db50f8efe6c3101189e9df02
Julien Huynh <julienhynh@gmail.com>
2024-09-11 11:15:35 +0200

    style result page

--- lib/pages/waether_result_page.dart
+++ lib/pages/waether_result_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:waestro_mobile/models/weather_model.dart';
+import 'package:intl/intl.dart';
+import 'package:waestro_mobile/models/weather_model.dart'; // Assurez-vous que vous avez bien ce modèle
+import 'package:waestro_mobile/styles/app_styles.dart'; // Import de AppStyle
 
 class WeatherResultPage extends StatelessWidget {
   final Weather weather;
@@ -9,45 +11,101 @@ class WeatherResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          AppStyle.backgroundColor, // Utilisation de la couleur de fond
       appBar: AppBar(
-        title: Text('Météo pour ${weather.cityName}'),
+        backgroundColor: Colors.transparent, // Rendre l'appbar transparent
+        iconTheme: IconThemeData(
+            color: AppStyle.primaryTextColor), // Couleur de l'icône
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Température actuelle : ${weather.temp}°C',
-              style: TextStyle(fontSize: 20),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Titre principal
+                Text(
+                  weather.cityName,
+                  style:
+                      AppStyle.cityNameStyle, // Style pour le nom de la ville
+                ),
+                SizedBox(height: 10),
+
+                // Température actuelle et ressenti
+                Text(
+                  '${weather.temp}°',
+                  style: AppStyle.tempStyle, // Style pour la température
+                ),
+                Text(
+                  'Real feel ${weather.feelslike}°',
+                  style: AppStyle.realFeelStyle, // Style pour le ressenti
+                ),
+                SizedBox(height: 20),
+
+                // Date d'aujourd'hui
+                Text(
+                  DateFormat('dd MMMM, EEEE')
+                      .format(DateTime.now()), // Date formatée
+                  style: AppStyle.dateStyle, // Style pour la date
+                ),
+                SizedBox(height: 20),
+              ],
             ),
-            Text(
-              'Ressenti : ${weather.feelslike}°C',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Prévisions :',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: weather.days.length,
-                itemBuilder: (context, index) {
-                  final day = weather.days[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(
-                        '${day.date.toLocal()} - Max: ${day.maxTemp}°C, Min: ${day.minTemp}°C',
-                      ),
-                      subtitle: Text('Condition : ${day.condition}'),
-                    ),
-                  );
-                },
+          ),
+
+          // Prévisions du jour et des jours suivants avec un fond en dégradé
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment(-1, 1),
+                  colors: <Color>[
+                    Color(0xFF101010), // Couleur de début
+                    Color.fromARGB(255, 38, 40, 55), // Couleur de fin
+                  ],
+                  tileMode: TileMode.mirror, // Mode de tuile
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: weather.days.length,
+                  itemBuilder: (context, index) {
+                    final day = weather.days[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          contentPadding: EdgeInsets.symmetric(vertical: 4.0),
+                          title: Text(
+                            '${DateFormat('dd MMMM').format(day.date)}', // Formatage de la date
+                            style: AppStyle
+                                .dayTitleStyle, // Style pour le titre du jour
+                          ),
+                          subtitle: Text(
+                            'Max: ${day.maxTemp}°C - Min: ${day.minTemp}°C',
+                            style: AppStyle
+                                .daySubtitleStyle, // Style pour le sous-titre
+                          ),
+                          trailing: Text(
+                            day.condition, // Condition météo
+                            style: AppStyle
+                                .conditionStyle, // Style pour la condition météo
+                          ),
+                        ),
+                        Divider(
+                            color:
+                                AppStyle.dividerColor), // Ligne de séparation
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Pour formater les dates
+
+class AppStyle {
+  // Couleurs
+  static const Color backgroundColor = Colors.black;
+  static const Color primaryTextColor = Colors.white;
+  static const Color secondaryTextColor = Colors.white70;
+  static const Color dividerColor = Colors.white24;
+
+  // Text styles
+  static const TextStyle cityNameStyle = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle tempStyle = TextStyle(
+    fontSize: 64,
+    fontWeight: FontWeight.bold,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle realFeelStyle = TextStyle(
+    fontSize: 16,
+    color: secondaryTextColor,
+  );
+
+  static const TextStyle dateStyle = TextStyle(
+    fontSize: 16,
+    color: secondaryTextColor,
+  );
+
+  static const TextStyle dayTitleStyle = TextStyle(
+    fontSize: 18,
+    color: primaryTextColor,
+  );
+
+  static const TextStyle daySubtitleStyle = TextStyle(
+    fontSize: 16,
+    color: secondaryTextColor,
+  );
+
+  static const TextStyle conditionStyle = TextStyle(
+    color: primaryTextColor,
+  );
+
+}
